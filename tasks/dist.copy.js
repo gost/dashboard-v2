5dc7dd802946db17e3ae77cfac5036e26b27e14f
time <ebben.tim@gmail.com>
2017-11-07 11:38:18 +0100

    Working on gulp dist

--- tasks/dist.copy.js
+++ tasks/dist.copy.js
@@ -8,7 +8,7 @@ const merge = require('merge-stream');
 
 module.exports = function(gulp) {
   return function() {
-    var outputDir = './build/es5-basic/';
+    var outputDir = './build/default/';
     // These directories contain files that are not included in the polymer build output for various reasons.
     // (Whenever possible files should be imported using HTML imports, so they're included in the polymer build.)
     var extraDirectories = [
@@ -20,8 +20,6 @@ module.exports = function(gulp) {
       'public/bower_components/px-dark-theme',
       'public/bower_components/px-vis',
       'public/bower_components/pxd3',
-
-      'public/elements/dev-guide'
     ];
 
     var extraStreams = [];
@@ -30,11 +28,9 @@ module.exports = function(gulp) {
       extraStreams.push(gulp.src([bc + '/**/*.*']).pipe(gulp.dest(outputDir + bc)));
     });
 
-    var docsFiles = gulp.src(['public/docs/**/*.*']).pipe(gulp.dest(outputDir + 'public/docs'));
-    var server = gulp.src(['server/**/*.*']).pipe(gulp.dest(outputDir + '/server'))
-    var packageFile = gulp.src(['package.json']).pipe(gulp.dest(outputDir));
-    var imageFiles = gulp.src(['public/images/**/*.*']).pipe(gulp.dest(outputDir + 'public/images'));
+    var imageFiles = gulp.src(['public/*_config.json']).pipe(gulp.dest(outputDir + 'public/'));
+    var configFiles = gulp.src(['public/images/**/*.*']).pipe(gulp.dest(outputDir + 'public/images'));
 
-    return merge(server, packageFile, extraStreams, docsFiles, imageFiles);
+    return merge(extraStreams, imageFiles, configFiles);
   };
 };
